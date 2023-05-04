7fa3c09e8fb73422a46d8faae917eeaaa32b8b34
Marie <muramatsumarie@gmail.com>
2023-05-04 21:58:32 +0900

    fix errors

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -25,47 +25,70 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-     
         
+    }
+    
+    func check(){
+        
+        if increaseNum < goalNum{
+            message = "足りない！"
+            msg.text = message
+            
+            
+        } else if increaseNum > goalNum{
+            message = "多すぎる！"
+            msg.text = message
+        }else{
+            message = "ピッタリ！"
+            msg.text = message
+                   
+        }
+
     }
     
     @IBAction func addOne(){
         
         increaseNum = increaseNum + 1
         increased.text = String(increaseNum)
-        
+        print(increaseNum)
+        check()
     }
     
     @IBAction func addFive(){
    
         increaseNum = increaseNum + 5
         increased.text = String(increaseNum)
-        
+        print(increaseNum)
+        check()
     }
     
     @IBAction func addTen(){
      
         increaseNum = increaseNum + 10
         increased.text = String(increaseNum)
-        
+        print(increaseNum)
+        check()
     }
     
     @IBAction func addFifty(){
      
         increaseNum = increaseNum + 50
         increased.text = String(increaseNum)
-        
+        print(increaseNum)
+        check()
     }
     
     @IBAction func addHundred(){
         
         increaseNum = increaseNum + 100
         increased.text = String(increaseNum)
-            
-        
+        print(increaseNum)
+        check()
     }
     
+   
     
+
 
 
 }
