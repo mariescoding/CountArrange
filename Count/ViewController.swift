fabe31310ca6c6c1692e2188741e7128db950569
Marie <muramatsumarie@gmail.com>
2023-05-04 20:01:23 +0900

    add files

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,19 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var number: Int = 0
+    var goalNum: Int = 349
+    var increaseNum : Int = 0
+    var message : String = ""
     
-    @IBOutlet var label: UILabel!
+    
+    @IBOutlet var increased: UILabel!
+    @IBOutlet var msg :UILabel!
+    @IBOutlet var one :UIButton!
+    @IBOutlet var five :UIButton!
+    @IBOutlet var ten :UIButton!
+    @IBOutlet var fifty :UIButton!
+    @IBOutlet var hundred :UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+     
+        
     }
     
-    @IBAction func plus(){
-        number = number + 1
-        label.text = String(number)
+    @IBAction func addOne(){
+        
+        increaseNum = increaseNum + 1
+        increased.text = String(increaseNum)
+        
     }
+    
+    @IBAction func addFive(){
+   
+        increaseNum = increaseNum + 5
+        increased.text = String(increaseNum)
+        
+    }
+    
+    @IBAction func addTen(){
+     
+        increaseNum = increaseNum + 10
+        increased.text = String(increaseNum)
+        
+    }
+    
+    @IBAction func addFifty(){
+     
+        increaseNum = increaseNum + 50
+        increased.text = String(increaseNum)
+        
+    }
+    
+    @IBAction func addHundred(){
+        
+        increaseNum = increaseNum + 100
+        increased.text = String(increaseNum)
+            
+        
+    }
+    
+    
 
 
 }
